8664c7b2b906dc018b1b9e2b350156e7f0c8ba6b
mingxu <3915327039@qq.com>
2026-09-23 12:14:28 +0800

    example

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main()
 {   
     int a,b;
     printf("Hello, xuming!\n");
+    printf("This is a simple C program.\n");
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     printf("%d + %d is %d\n", a, b, a + b);
