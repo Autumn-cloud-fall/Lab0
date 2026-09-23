7b7b5d64210f230f7cbb87b1a47e31b7f43215df
mingxu <3915327039@qq.com>
2026-09-23 12:17:59 +0800

    main1

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ int main()
 {   
     int a,b;
     printf("Hello, xuming!\n");
-    printf("This is a simple C program.\n");
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     printf("%d + %d is %d\n", a, b, a + b);
