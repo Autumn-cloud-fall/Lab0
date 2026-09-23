f5e525c6c987ac0302607210937032b5238778e3
mingxu <3915327039@qq.com>
2026-09-23 11:46:31 +0800

    text2

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main()
-{
+{   
+    // @TODO: print a sentence you want.
+    int a,b;
     printf("Hello, world!\n");
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("%d + %d is %d\n", a, b, a + b);
+    return 0;
 }
